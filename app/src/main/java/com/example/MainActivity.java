2f1189312e725a4ac5b4f4421ac183c9419130bc
shivasubedi <cvaprince15@gmail.com>
2019-12-12 21:39:48 +0545

    softwarica login

---
+++ app/src/main/java/com/example/MainActivity.java
@@ -0,0 +1,52 @@
+package com.example;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.example.MainActivity;
+import com.example.wbv.R;
+import com.example.wbv.home;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private EditText UserName;
+    private EditText Password;
+    private Button Login;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        UserName=(EditText)findViewById(R.id.etName);
+        Password=(EditText)findViewById(R.id.etPassword);
+        Login=(Button)findViewById(R.id.btnLogin);
+
+        Login.setOnClickListener(this);
+
+    }
+
+
+    @Override
+    public void onClick(View v) {
+
+        String username = UserName.getText().toString();
+        String password =Password.getText().toString();
+        if(username.equals("softwarica") && password.equals("coventry"))
+        {
+            Intent i = new Intent(this, home.class);
+            startActivity(i);
+        }
+        else
+        {
+            Toast.makeText(getApplicationContext(),"Password not match !!",Toast.LENGTH_SHORT).show();
+        }
+    }
+}
